674dc2071916b036dbb9f0bea64782d8448a9721
yeonhee <cyh2616@naver.com>
2024-10-21 20:59:38 +0900

    SignUp 타입 페이지_1

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavBar from "./component/Navbar/Navbar";
 import Main from "./component/Main/Main";
 import Login from "./component/Login/Login";
+import SignUp from "./component/SignUp/SignUp";
 import "./App.css";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
           {/* Add other routes as needed */}
         </Routes>
       </div>
---
+++ src/component/SignUp/SignUp.js
@@ -0,0 +1,38 @@
+// SignUpType.js
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import styles from './SignUp.module.css';
+
+const SignUp = () => {
+  const navigate = useNavigate();
+
+  const handleTypeSelection = (type) => {
+    navigate(`/signup/${type}`);
+  };
+
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>SIGN UP</h1>
+        <div className={styles.signUpBox}>
+          <div className={styles.buttonGroup}>
+            <button 
+              className={styles.typeButton} 
+              onClick={() => handleTypeSelection('member')}
+            >
+              회원용
+            </button>
+            <button 
+              className={styles.typeButton} 
+              onClick={() => handleTypeSelection('counselor')}
+            >
+              상담사용
+            </button>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default SignUp;
