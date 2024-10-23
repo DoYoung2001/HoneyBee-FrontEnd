70857448158d73161dcbbd112904488ed19b4320
yeonhee <cyh2616@naver.com>
2024-10-23 13:57:08 +0900

    counselordetail 페이지_1

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Member from "./component/SignUpMember/Member";
 import Counselor from "./component/SignUpCounselor/Counselor";
 import "./App.css";
 import CounselorList from "./component/CounselorList/CounselorList";
+import CounselorDetail from "./component/CounselorDetail/CounselorDetail";
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
           <Route path="/signup/member" element={<Member />} /> 
           <Route path="/signup/counselor" element={<Counselor />} /> 
           <Route path="/counselorlist" element={<CounselorList />} />
+          <Route path="/counselordetail" element={<CounselorDetail />} />
           
         </Routes>
       </div>
