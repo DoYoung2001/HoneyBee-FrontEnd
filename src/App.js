c7c2bd6b9b16c90879dec011062b5dd4ff0463d9
hye won <hw2938@gmail.com>
2024-10-25 01:36:27 +0900

    App.js 전체 페이지 전환 시 스크롤이 무조건 위로 위치하게 설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React, { useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
 import NavBar from "./component/Navbar/Navbar";
 import Main from "./component/Main/Main";
 import Mypage from "./component/Mypage/Mypage";
@@ -17,12 +22,23 @@ import QuestionBoard from "./component/QuestionBoard/QuestionBoard";
 import CreatePost from "./component/CreatePost/CreatePost";
 import LikeCounselor from "./component/LikeCounselor/LikeCounselor";
 
+// 스크롤 위치를 조정하는 컴포넌트
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0); // 페이지가 변경될 때 스크롤을 맨 위로 이동
+  }, [pathname]);
+
+  return null; // 이 컴포넌트는 아무것도 렌더링하지 않음
+};
 
 function App() {
   return (
     <Router>
       <div className="App">
         <NavBar />
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/mypage" element={<Mypage />} />
@@ -32,7 +48,10 @@ function App() {
           <Route path="/signup/member" element={<Member />} />
           <Route path="/signup/counselor" element={<Counselor />} />
           <Route path="/counselorlist" element={<CounselorList />} />
-          <Route path="/counselorlist/counselordetail/:id" element={<CounselorDetail />} />
+          <Route
+            path="/counselorlist/counselordetail/:id"
+            element={<CounselorDetail />}
+          />
           <Route path="/createpost" element={<CreatePost />} />
           <Route path="/questionboard" element={<QuestionBoard />} />
           <Route path="/likecounselor" element={<LikeCounselor />} />
